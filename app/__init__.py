0880acbcc8bd8e268cbcaff129283e639dcaacba
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-11-23 17:10:50 +0000

    Updated the db setup script

--- app/__init__.py
+++ app/__init__.py
@@ -7,15 +7,12 @@ from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 from dotenv import load_dotenv
 from app.db_storage import DBStorage
-from app import dog_routes, models, routes
+
 # from flask_babel import Babel
 
 app = Flask(__name__)
 
 load_dotenv()
-
-csrf = CSRFProtect(app)
-db_storage = DBStorage(app)
 
 username = os.getenv('MYSQL_USER')
 password = os.getenv('MYSQL_PWD')
@@ -31,3 +28,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Explicitly set the time zone
 # app.config['BABEL_DEFAULT_LOCALE'] = 'en_US'  # Use your desired locale
 # app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'  # Use your desired time zone
+
+csrf = CSRFProtect(app)
+db_storage = DBStorage(app)
+
+from app import dog_routes, models, routes
--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,7 @@
 -- prepares a MySQL server for the project
 
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+CREATE DATABASE IF NOT EXISTS pp_dev_db;
+CREATE USER IF NOT EXISTS 'pp_user'@'localhost' IDENTIFIED BY 'pp_PWD_167,@9uri0';
+GRANT ALL PRIVILEGES ON `pp_dev_db`.* TO 'pp_user'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'pp_user'@'localhost';
 FLUSH PRIVILEGES;
