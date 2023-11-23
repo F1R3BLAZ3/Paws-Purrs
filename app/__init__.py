bbf4935c799c07f8d24307968b362ab33b4dc351
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-11-23 16:47:52 +0000

    merge conflicts

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,6 @@ load_dotenv()
 app = Flask(__name__)
 
 from app import dog_routes, models
-# from flask_babel import Babel
 
 load_dotenv()
 
@@ -26,13 +25,12 @@ password = os.getenv('MYSQL_PWD')
 host = os.getenv('MYSQL_HOST')
 dbname = os.getenv('MYSQL_DB')
 
-# Explicitly set the time zone
-# app.config['BABEL_DEFAULT_LOCALE'] = 'en_US'  # Use your desired locale
-# app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'  # Use your desired time zone
-
 # Configuration variables
 app.config['DOG_API_BASE_URL'] = 'https://api.thedogapi.com/v1'
 app.config['DOG_API_KEY'] = os.getenv('DOG_API_KEY')
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{username}:{password}@{host}/{dbname}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Explicitly set the time zone
+# app.config['BABEL_DEFAULT_LOCALE'] = 'en_US'  # Use your desired locale
+# app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'  # Use your desired time zone
