43f1b53901ae524bc7dd0359e4135b95ce3656dd
F1R3BLAZ3 <zandre.777.deboer@gmail.com>
2023-11-28 19:46:40 +0000

    Updated __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -12,16 +12,16 @@ load_dotenv()
 
 app = Flask(__name__)
 
-# Explicitly set the time zone
-# app.config['BABEL_DEFAULT_LOCALE'] = 'en_US'  # Use your desired locale
-# app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'  # Use your desired time zone
-
 # Configuration variables
 app.config['DOG_API_BASE_URL'] = 'https://api.thedogapi.com/v1'
 app.config['DOG_API_KEY'] = os.getenv('DOG_API_KEY')
 app.config['CAT_API_BASE_URL'] = 'https://api.thecatapi.com/v1'
 app.config['CAT_API_KEY'] = os.getenv('CAT_API_KEY')
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+# Explicitly set the time zone
+# app.config['BABEL_DEFAULT_LOCALE'] = 'en_US'  # Use your desired locale
+# app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'  # Use your desired time zone
 
 csrf = CSRFProtect(app)
 
